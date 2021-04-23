f939e051b321063af9f8b22a178ae20869e81b89
stillnurs <noorsultan.mamataliev@gmail.com>
2021-04-23 17:37:00 +0400

    created home app, added basic configurations

--- shopify_django_app/shopify_django_app/settings.py
+++ shopify_django_app/shopify_django_app/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from shopify_app import *
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l$y1io0pxy=c0(fw6xb*lj@-8xe7_pqiq$gcyfal0wu&n!sm5v'
+SECRET_KEY = os.environ.get('DJANGO_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'shopify_app.apps.ShopifyAppConfig',
+    'home.apps.HomeConfig',
 ]
 
 MIDDLEWARE = [
